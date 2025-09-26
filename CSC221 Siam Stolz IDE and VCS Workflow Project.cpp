6487b0cf3f0912204a4f6ab8ffdd5fb0dbdc4761
Jasmine <Jasmine@LUMITY>
2025-09-26 01:07:08 -0400

    Add project files.

---
+++ CSC221 Siam Stolz IDE and VCS Workflow Project.cpp
@@ -0,0 +1,14 @@
+// IDE and VCS Workflow Project.cpp
+// This program displays your initials
+
+#include <iostream>
+using namespace std;
+int main() {
+	char first, middle, last;
+	//Store your initial in these variables
+	first = 'S';
+	middle = 'J';
+	last = 'S';
+	cout << first << middle << last << endl;
+	return 0;
+}
